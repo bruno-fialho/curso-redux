ce9b373128e0b5f0bae4354a20106eabee70b0d7
bruno-fialho <brugerf@gmail.com>
2021-02-09 21:18:34 -0300

    Add out of stock message

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
@@ -10,6 +11,10 @@ interface CatalogItemProps {
 
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
 
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product));
@@ -23,6 +28,10 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       <button type="button" onClick={handleAddProductToCart}>
         Comprar
       </button>
+
+      {hasFailedStockCheck && (
+        <span style={{ color: 'red' }}>Falta de estoque</span>
+      )}
     </article>
   );
 };
