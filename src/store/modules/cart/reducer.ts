f13bd6e874b4a5642431dbc001302959610d54b8
bruno-fialho <brugerf@gmail.com>
2021-02-09 21:37:31 -0300

    Add action types

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -2,7 +2,7 @@
 /* eslint-disable consistent-return */
 import { Reducer } from 'redux';
 import produce from 'immer';
-import { ICartState } from './types';
+import { ActionTypes, ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
   items: [],
@@ -12,7 +12,7 @@ const INITIAL_STATE: ICartState = {
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch (action.type) {
-      case 'ADD_PRODUCT_TO_CART_SUCCESS': {
+      case ActionTypes.addProductToCartSuccess: {
         const { product } = action.payload;
 
         const productInCartIndex = draft.items.findIndex(
@@ -30,7 +30,7 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
         break;
       }
-      case 'ADD_PRODUCT_TO_CART_FAILURE': {
+      case ActionTypes.addProductToCartFailure: {
         draft.failedStockCheck.push(action.payload.productId);
 
         break;
